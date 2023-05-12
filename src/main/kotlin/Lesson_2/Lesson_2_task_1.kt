73552896f960b1da17f03e01fd8a98ba6ea56606
Ksenia <ksuxahs@yandex.ru>
2023-05-12 12:46:16 +0300

    KS-2-1 print data from second lesson

--- src/main/kotlin/Lesson_2/Lesson_2_task_1.kt
+++ src/main/kotlin/Lesson_2/Lesson_2_task_1.kt
@@ -3,8 +3,8 @@ package Lesson_2
 fun main() {
     val a : Int = 3
     val b : Int = 4
-    val c : Int= 3
-    val d : Int= 5
+    val c : Int = 3
+    val d : Int = 5
     val sum :Int = a + b + c + d
     val numberOfStudents : Float = 4f
     println(sum / numberOfStudents)
