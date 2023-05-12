834222cc7703dbea43a4a84205b2caa88b77421a
Ksenia <ksuxahs@yandex.ru>
2023-05-12 09:57:27 +0300

    KS-1-1 print data from first lesson

---
+++ src/main/kotlin/Lesson_1/Lesson_1_task_1.kt
@@ -0,0 +1,8 @@
+package Lesson_1
+
+fun main () {
+    val numberOrder : Int = 75
+    val thankfulness : String = "Спасибо за покупку лесных жаб!"
+    println(numberOrder)
+    println(thankfulness)
+}
